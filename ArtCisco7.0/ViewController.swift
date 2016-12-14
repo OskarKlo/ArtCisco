2295501bb0dbf79b2f4b3a372cedcf8e0dfbcab4
Oskar Klonowski <partyzancik233@gmail.com>
2016-12-14 12:19:47 -0800

    Initial Commit

---
+++ ArtCisco7.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtCisco7.0
+//
+//  Created by Oskar on 12/14/16.
+//  Copyright © 2016 Oskar/Jake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ArtCisco7.0Tests/ArtCisco7_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ArtCisco7_0Tests.swift
+//  ArtCisco7.0Tests
+//
+//  Created by Oskar on 12/14/16.
+//  Copyright © 2016 Oskar/Jake. All rights reserved.
+//
+
+import XCTest
+@testable import ArtCisco7_0
+
+class ArtCisco7_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
