c445d8dda9f468d844a57a62537d967ebe7eefdd
Oskar2012 <oskark@thinkglobalschool.com>
2016-12-17 18:44:39 +0100

    adding the FBSDK

--- ArtCisco7.0/AppDelegate.swift
+++ ArtCisco7.0/AppDelegate.swift
@@ -29,6 +29,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        // Add any custom logic here.
+        return handled
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
