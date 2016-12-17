a192f5c046613dc8ca741b8e3f27368940fae8f0
Jake Ols <jake_ols@live.com>
2016-12-16 19:42:10 -0500

    cleanup for restart

--- ArtCisco7.0/ViewController.swift
+++ ArtCisco7.0/ViewController.swift
@@ -10,27 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signInButton: UIButton!
-    @IBOutlet weak var forgotButton: UIButton!
-    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func forgotPasswordButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "forgotPasswordSegue", sender: self)
-    }
-    
-    @IBAction func signInButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "homePageSegue", sender: self)
-    }
-
-    @IBAction func signUpButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "newAccountSegue", sender: self)
-    }
 
 }
 
