6176095bf1dbb3fc6310de31eba3b3c4481e37fc
Oskar Klonowski <partyzancik233@gmail.com>
2016-12-17 02:40:35 +0100

    registration page

---
+++ ArtCisco7.0/RegistrationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationViewController.swift
+//  ArtCisco7.0
+//
+//  Created by Oskar on 12/17/16.
+//  Copyright © 2016 Oskar/Jake. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ArtCisco7.0/ViewController.swift
+++ ArtCisco7.0/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func signUpButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "registrationSegue", sender: self)
+    }
 
 }
 
