02ef226ebc370a0039b0496b39427e6dc0c6ee6e
OskarKlo <oskark@thinkglobalschool.com>
2016-12-25 23:07:54 +0100

    working on the search bar

--- ArtCisco7.0/AppleMapViewController.swift
+++ ArtCisco7.0/AppleMapViewController.swift
@@ -11,17 +11,11 @@ import UIKit
 import MapKit
 import CoreLocation
 
-protocol HandleMapSearch {
-    func dropPinZoomIn(placemark:MKPlacemark)
-}
-
 class AppleMapViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     let manager = CLLocationManager()
-    var resultSearchController:UISearchController? = nil
-    var selectedPin:MKPlacemark? = nil
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
@@ -37,10 +31,6 @@ class AppleMapViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
-        return CGRect(x: x, y: y, width: width, height: height)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,30 +38,6 @@ class AppleMapViewController: UIViewController, CLLocationManagerDelegate {
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-        
-        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as!LocationSearchTable
-        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
-        resultSearchController?.searchResultsUpdater = locationSearchTable
-        
-        let searchBar = resultSearchController!.searchBar
-        searchBar.sizeToFit()
-        searchBar.placeholder = "Search for places"
-        navigationItem.titleView = resultSearchController?.searchBar
-        
-        resultSearchController?.hidesNavigationBarDuringPresentation = false
-        resultSearchController?.dimsBackgroundDuringPresentation = true
-        definesPresentationContext = true
-        
-        locationSearchTable.mapView = mapView
-        
-        locationSearchTable.handleMapSearchDelegate = self
-        
-        let button   = UIButton(type: UIButtonType.system) as UIButton
-        button.frame = CGRectMake(100, 100, 100, 50)
-        button.backgroundColor = UIColor.green
-        button.setTitle("Button", for: UIControlState.normal)
-        button.addTarget(self, action: Selector(("Action:")), for: UIControlEvents.touchUpInside)
-        self.view.addSubview(button)
         
     }
     override func didReceiveMemoryWarning() {
